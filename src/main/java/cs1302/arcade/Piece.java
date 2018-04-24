ac304ae455d29a6a1897735e971344cb462f4059
Dakota <sdvickers98@gmail.com>
2018-04-24 00:57:19 -0400

    testing the commit command

--- src/main/java/cs1302/arcade/Piece.java
+++ src/main/java/cs1302/arcade/Piece.java
@@ -8,6 +8,7 @@ public class Piece extends Rectangle {
 
     private static final int PIECE_SIZE = 20;
     
+    
     public Piece(Color color) {
 	super(PIECE_SIZE, PIECE_SIZE, color);
     } // Piece
